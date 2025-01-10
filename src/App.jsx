2cef9022b6532a2728e3c2061933bf60b4092154
ksetrin <ksetrinn@gmail.com>
2025-01-11 01:38:23 +0500

    use I18nextProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import ArticlesPage from './pages/ArticlesPage';
@@ -8,17 +10,19 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/articles" element={<ArticlesPage />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/articles" element={<ArticlesPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </I18nextProvider>
   );
 };
 
