9fe1a8fb2866f36f8a006778883cdc92b85466b8
ksetrin <ksetrinn@gmail.com>
2025-01-12 02:13:31 +0500

    tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,30 @@ export default {
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backdropBlur: {
         xs: '2px',
+      },
+      colors: {
+        dark: {
+          bg: '#121212',
+          surface: '#1E1E1E',
+          primary: '#bb86fc',
+          text: {
+            primary: '#ffffff',
+            secondary: '#a0aec0',
+          }
+        },
+      },
+      backgroundColor: {
+        primary: 'var(--bg-primary)',
+        secondary: 'var(--bg-secondary)',
+      },
+      textColor: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
       },
     },
   },
