70abf942aceed33b0b5e441543a66b89a33933f8
ksetrin <ksetrinn@gmail.com>
2025-01-14 02:15:27 +0500

    use ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+import { ThemeProvider } from './context/ThemeContext';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import ArticlesPage from './pages/ArticlesPage';
@@ -11,17 +12,19 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <I18nextProvider i18n={i18n}>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/articles" element={<ArticlesPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <ThemeProvider>
+        <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-200">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/articles" element={<ArticlesPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </ThemeProvider>
     </I18nextProvider>
   );
 };
