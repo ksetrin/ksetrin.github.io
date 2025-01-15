01ef687833698c0d680f519d28c1fb385af695b3
ksetrin <ksetrinn@gmail.com>
2025-01-16 02:52:11 +0500

    upd vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/ksetrin.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+  },
 });
